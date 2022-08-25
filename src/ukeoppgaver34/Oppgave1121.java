ae25de4216c4f5bf605b4fd8160de3b257ab61f9
Sultan Avtajev <sultan_avtajev@hotmail.com>
2022-08-25 21:35:28 +0200

    Fortsettelse på oppgaver for uke 34

---
+++ src/ukeoppgaver34/Oppgave1121.java
@@ -0,0 +1,20 @@
+package ukeoppgaver34;
+
+//Min metode som henter posisijonen til den laveste verdien i et array og skriver denne ut til console
+
+public class Oppgave1121 {
+    static int min(int[] a) {
+        int minst = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < a[minst]) {
+                minst = a[i];
+            }
+        }
+        return minst;
+    }
+    public static void main(String[] args) {
+        int [] a= {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
+        System.out.println(min(a));
+    }
+}
--- src/ukeoppgaver34/Oppgave3.java
+++ src/ukeoppgaver34/Oppgave3.java
@@ -1,7 +1,9 @@
 package ukeoppgaver34;
 
-public class Oppgave3 {
+
+class Oppgave3 {
     public static void main(String[] args) {
-        System.out.println("Hei AlgDat!");
+        int [] a= {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
+        System.out.println(Oppgave1121.min(a));
     }
 }
